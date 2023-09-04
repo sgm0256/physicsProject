125bb6b86a8a5ef85f18d7a36704f437495f37a1
sgm0256 <Gubbojoky0526@gmail.com>
2023-09-04 23:53:52 +0900

    LifeBar Complete

--- Assets/01.Scripts/Magnet.cs
+++ Assets/01.Scripts/Magnet.cs
@@ -39,5 +39,53 @@ public class Magnet : MonoBehaviour
                 tween = transform.DOMoveY(transform.position.y - 2.5f, 0.2f);
             }
         }
+
+        // Test
+        /*if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (tween != null && tween.IsActive()) return;
+            if (transform.position.y <= 1f)
+            {
+                if (spriteRenderer.sprite == magnetSpr[0])
+                    sgm_GameManager.Instance.Plus();
+                else sgm_GameManager.Instance.Minus();
+                tween = transform.DOMoveY(-6.5f, 0.2f).OnComplete(() =>
+                {
+                    transform.position = spawnTrm.position;
+                    int ran = Random.Range(1, 3);
+                    if (ran == 1)
+                        spriteRenderer.sprite = magnetSpr[0];
+                    else
+                        spriteRenderer.sprite = magnetSpr[1];
+                });
+            }
+            else
+            {
+                tween = transform.DOMoveY(transform.position.y - 2.5f, 0.2f);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (tween != null && tween.IsActive()) return;
+            if (transform.position.y <= 1f)
+            {
+                if (spriteRenderer.sprite == magnetSpr[1])
+                    sgm_GameManager.Instance.Plus();
+                else sgm_GameManager.Instance.Minus();
+                tween = transform.DOMoveY(-6.5f, 0.2f).OnComplete(() =>
+                {
+                    transform.position = spawnTrm.position;
+                    int ran = Random.Range(1, 3);
+                    if (ran == 1)
+                        spriteRenderer.sprite = magnetSpr[0];
+                    else
+                        spriteRenderer.sprite = magnetSpr[1];
+                });
+            }
+            else
+            {
+                tween = transform.DOMoveY(transform.position.y - 2.5f, 0.2f);
+            }
+        }*/
     }
 }
--- Assets/01.Scripts/sgm_GameManager.cs
+++ Assets/01.Scripts/sgm_GameManager.cs
@@ -1,11 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class sgm_GameManager : MonoSingleton<sgm_GameManager>
 {
-    public float socre { get; private set; }
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private GameObject panel;
 
+    private float divideValue = 100f;
+    private float value = 0.15f;
 
+    public float Score { get; private set; }
+
+    private void Update()
+    {
+        //Test
+        /*if(slider.value == 0)
+        {
+            panel.SetActive(true);
+        }*/
+
+        value += Time.deltaTime / divideValue;
+        MinusLife(value);
+    }
+
+    public void Plus()
+    {
+        Score += 10;
+        slider.value += 15 + value * 15f;
+    }
+
+    public void Minus()
+    {
+        slider.value -= 15 + value * 35f;
+        //StartCoroutine("PanelActive"); Test
+    }
+
+    private void MinusLife(float value)
+    {
+        slider.value -= value;
+    }
+
+    //Test
+    /*IEnumerator PanelActive()
+    {
+        panel.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        panel.SetActive(false);
+    }*/
 }
