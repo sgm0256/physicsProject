02c7c0918e8a7844422f10ab0f43f57b58844639
sgm0256 <Gubbojoky0526@gmail.com>
2023-09-04 16:16:43 +0900

    magnet Complete

--- Assets/01.Scripts/Magnet.cs
+++ Assets/01.Scripts/Magnet.cs
@@ -5,18 +5,39 @@ public class Magnet : MonoBehaviour
 {
     [SerializeField]
     private Transform spawnTrm;
+    [SerializeField]
+    private Sprite[] magnetSpr;
+
+    SpriteRenderer spriteRenderer;
+    private Tween tween;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            transform.DOMoveY(transform.position.y - 2.5f, 0.2f);
-        }
-
-        if(transform.position.y <= -6.5f)
-        {
-            transform.position = spawnTrm.position;
+            if (tween != null && tween.IsActive()) return;
+            if (transform.position.y <= 1f)
+            {
+                tween = transform.DOMoveY(-6.5f, 0.2f).OnComplete(() =>
+                {
+                    transform.position = spawnTrm.position;
+                    int ran = Random.Range(1, 3);
+                    if (ran == 1)
+                        spriteRenderer.sprite = magnetSpr[0];
+                    else
+                        spriteRenderer.sprite = magnetSpr[1];
+                });
+            }
+            else
+            {
+                tween = transform.DOMoveY(transform.position.y - 2.5f, 0.2f);
+            }
         }
     }
 }
---
+++ Assets/01.Scripts/sgm_GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sgm_GameManager : MonoSingleton<sgm_GameManager>
+{
+    public float socre { get; private set; }
+
+
+}
